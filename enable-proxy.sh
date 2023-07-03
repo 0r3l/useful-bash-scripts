c88ccfcd0f0122e1fb946b22aee99f97db089a1c
Orel Elya <oe530k@intl.att.com>
2023-07-03 16:52:21 +0300

    bugs fix

--- enable-proxy.sh
+++ enable-proxy.sh
@@ -1,15 +1,13 @@
-#!/bin/sh
+#!/bin/zsh
 
-function enableproxy() {
-  proxy_url=http://sub.proxy.att.com:8080/
-  export HTTP_PROXY=${proxy_url}
-  export HTTPS_PROXY=${proxy_url}
-  export http_proxy=$HTTP_PROXY
-  export https_proxy=$HTTPS_PROXY
-  export no_proxy=localhost  
-  git config --global http.proxy ${proxy_url}
-  git config --global https.proxy ${proxy_url}
-  npm config set proxy ${proxy_url}
-}
-
-enableproxy
+proxy_url=http://01uklopxy00.pst.cso.att.com:8080
+echo "export $proxy_url to HTTP_PROXY,HTTPS_PROXY,http_proxy,https_proxy no_proxy=localhost"
+export PATH=$HOME/development/flutter/bin:$PATH
+export HTTP_PROXY=${proxy_url}
+export HTTPS_PROXY=${proxy_url}
+export http_proxy=$HTTP_PROXY
+export https_proxy=$HTTPS_PROXY
+export no_proxy=localhost
+git config --global http.proxy ${proxy_url}
+git config --global https.proxy ${proxy_url}
+npm config set proxy ${proxy_url}
--- kubernetees.md
+++ kubernetees.md
@@ -4,3 +4,15 @@ kubectl exec -it -n igw-platform -c igw-common-auth $(kubectl get po -n igw-plat
 kubectl logs -n igw-platform -c igw-common-auth $(kubectl get po -n igw-platform | grep igw-common-auth | cut -d" " -f1) -f
 ## restart pod
 kubectl rollout restart deployment -n igw-platform igw-common-auth
+## running pods
+kubectl get po -n igw-platform 
+## create secret
+kubectl create secret generic <secret_name> --from-literal=<key>=<Value> -n <Namespace>
+## describe secret 
+kubectl describe secret <secret_name> -n igw-platform
+## change env vars
+kubectl edit deployment -n igw-platform igw-common-auth
+# get secrets list
+kubectl get secret <name> -n igw-platform
+## edit secret
+kubectl edit secrets <name> -n igw-platform
